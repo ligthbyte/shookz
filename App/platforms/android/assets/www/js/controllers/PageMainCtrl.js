48b09bfc9914084c239a8d7272f53436d89daad3
Kobi Shonberger <info@lightbyte.co.il>
2018-03-14 23:52:58 +0200

    some swiper updates + activate swiper

--- App/platforms/android/assets/www/js/controllers/PageMainCtrl.js
+++ App/platforms/android/assets/www/js/controllers/PageMainCtrl.js
@@ -1,3 +1,11 @@
 app.controller('PageMainCtrl', function($scope){
-    
+    //init swiper
+    var mySwiper = new Swiper('.swiper-container', {  
+        spaceBetween: 10,  
+        observer: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+      })
 });
